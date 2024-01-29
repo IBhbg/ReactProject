21f9ca73beb3a89a33dc05bf34921b592763b1c8
Ivan Badal <117454160+IBhbg@users.noreply.github.com>
2024-01-29 15:52:32 +0100

    Refactor code to improve performance and readability

---
+++ my-react-less-app/src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import './App.less'; // Import the LESS file
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* ... your existing JSX code ... */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
