2c330ba9d56ebda693ac4d8b52780dc44769ecf5
Ivan Badal <117454160+IBhbg@users.noreply.github.com>
2024-01-29 15:48:38 +0100

    Updated .gitignore

--- my-react-less-app/src/App.test.js
+++
@@ -1,9 +0,0 @@
-// App.test.js
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders App-header', () => {
-  render(<App />);
-  const headerElement = screen.getByRole('banner');
-  expect(headerElement).toBeInTheDocument();
-});
